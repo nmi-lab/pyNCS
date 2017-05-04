2abe2ce8052d02c637b3839bec051f6c663dcb0c
Emre Neftci <eneftci@uci.edu>
2017-05-04 13:40:47 -0700

    some renaming and adding new functions

--- test/expCom.py
+++ test/expCom.py
@@ -16,19 +16,19 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('128.200.83.67', 8890))
 sock.settimeout(1.0)
 
-def clear_sram_memory(sramId = 1, coreId = 0, chipid = 0):
+def clear_sram_memory(sramId = 1, coreId = 0, chipId = 0):
     for neuronId in range(256):
         #we loop over all cores
         dx = 0;
         sx = 0;
         dy = 0;
         sy = 0;
-        destcoreId = 0;        
-        bits = neuronId << 7 | sramId << 5 | coreId << 15 | 1 << 17 | 1 << 4 | destcoreId << 18 | sy << 27 | dy << 25 | dx << 22 | sx << 24 | coreId << 28 | chipid <<30;
+        destcoreId = 0;  #one hot coded    
+        bits = neuronId << 7 | sramId << 5 | coreId << 15 | 1 << 17 | 1 << 4 | destcoreId << 18 | sy << 27 | dy << 25 | dx << 22 | sx << 24 | coreId << 28 | chipId <<30;
         send_dynapse_event(bits)
 
 def set_neurons_sram(
-        chipid,
+        chipId,
         coreId,
         sramId = 1,
         neurons = range(256),
@@ -41,59 +41,78 @@ def set_neurons_sram(
 
     for neuronId in neurons:
         #we loop over all cores
-        bits = neuronId << 7 | sramId << 5 | coreId << 15 | 1 << 17 | 1 << 4 | destcoreId << 18 | sy << 27 | dy << 25 | dx << 22 | sx << 24 | coreId << 28 | chipid <<30;
+        bits = neuronId << 7 | sramId << 5 | coreId << 15 | 1 << 17 | 1 << 4 | destcoreId << 18 | sy << 27 | dy << 25 | dx << 22 | sx << 24 | coreId << 28 | chipId <<30;
         send_dynapse_event(bits)
 
 def clear_camId(
-        chipid,
+        chipId,
         coreId,
         camId,
         neuronId = 0):
-    bits = chipid << 30 | 1 << 17 |coreId << 15 | neuronId<<5 ;
+    bits = chipId << 30 | 1 << 17 |coreId << 15 | neuronId<<5 ;
     send_dynapse_event(bits)
 
-def clear_core_cam( chipid,
+def clear_core_cam( chipId,
                     coreId,
                     ):
     for row in range(1024):
         bits = []
         for col in range(16):
-            bits.append(chipid << 30 | 1 << 17 |coreId << 15 | row <<5 | col );
+            bits.append(chipId << 30 | 1 << 17 |coreId << 15 | row <<5 | col );
         send_dynapse_event(bits)
 
 def set_neuron_cam(
-        chipid,
+        chipId,
         camId,
-        ei = 1,
-        fs = 1,
-        srcneuronId = 0,
-        destneuronId = 0,
-        srccoreId = 0,
-        destcoreId = 0):
+        ei = 1, #excitatory
+        fs = 1, #fast
+        srcneuronId = 0, #sending neuron
+        destneuronId = 0, #receiving neuron
+        srccoreId = 0, #sending core (= extratag)
+        destcoreId = 0): #receiving core (not 1 hot coded)
     bits = []
     synapse_row = camId;                 # cam ID
     nrn_1 = (destneuronId & 0xf0)>>4
     nrn_2 = destneuronId & 0x0f
-    bits .append( chipid << 30 | ei << 29 | fs << 28 | srcneuronId << 20 | srccoreId << 18 | 1 << 17 | destcoreId << 15 | nrn_1 << 11 | camId << 5 | nrn_2 );
+    bits .append( chipId << 30 | ei << 29 | fs << 28 | srcneuronId << 20 | srccoreId << 18 | 1 << 17 | destcoreId << 15 | nrn_1 << 11 | camId << 5 | nrn_2 );
     send_dynapse_event(bits)
 
-
-
-
 def send_dynapse_event(events):
-    sock.send(np.array(events).astype('uint64').tobytes())
+    sock.send(np.array(events).astype('uint64').tostring())
+
+def tau1_core_set(chipId,coreId):
+    bits = chipId << 30 |\
+            1 << 12 |\
+            0 << 11 |\
+            coreId << 8
+    send_dynapse_event(bits)
+
+def tau2_core_set(chipId,coreId):
+    bits = chipId << 30 |\
+            1 << 12 |\
+            1 << 11 |\
+            coreId << 8
+    send_dynapse_event(bits)
+
+def tau2_set(chipId,coreId,neuronId):
+    bits = chipId << 30 |\
+            1 << 10 |\
+            coreId << 8 |\
+            neuronId
+    send_dynapse_event(bits)
 
 if __name__ == '__main__':
-    clear_sram_memory(sramId=1,coreId=3,chipid=0)
-    set_neurons_sram(chipid=0, coreId=0,sramId=1,neurons=range(256), destcoreId=8)
-    #core 3 is 0b1000 = 3
-    for j in range(0,16<<4,16):
-        for i in range(32):
-            set_neuron_cam(chipid=0,
-                    camId=i,
-                    ei=1,
-                    fs=1,
-                    srcneuronId=i,
-                    destneuronId=j,
-                    srccoreId=0,
-                    destcoreId=3)
+    clear_sram_memory(sramId=1,coreId=2,chipId=0)
+    set_neurons_sram(chipId=0, coreId=2,sramId=1,neurons=range(256), destcoreId=4)
+    #core 3 is 0b1000 = 8
+    for j in range(0,256):
+        for i in range(0,256):
+            if (j&16)==(i&16)+1:
+                set_neuron_cam(chipId=4,
+                        camId=i,
+                        ei=1,
+                        fs=1,
+                        srcneuronId=i,
+                        destneuronId=j,
+                        srccoreId=2,
+                        destcoreId=2)
