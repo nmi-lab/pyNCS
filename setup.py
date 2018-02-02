91cf31030eddc2fa3246e1436139b060a6641dee
Emre Neftci <eneftci@uci.edu>
2018-02-02 13:02:23 -0800

    updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='pyNCSre',
-	version='20180126',
+	version='20180202_0',
 	description='Python Neurormophic Chips and Systems, reworked',
 	author='Emre Neftci',
 	author_email='eneftci@uci.edu',
